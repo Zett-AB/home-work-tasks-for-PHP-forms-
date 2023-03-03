948a110ebfbfba8dcc89ac2329ff05ec7f448e6a
Alexander <formysite.ru@yandex.ru>
2023-03-03 13:11:44 +0300

    step1

---
+++ player.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Work PHP 1</title>
+</head>
+
+<body>
+    <main>
+        <header>
+            <h1 class="title">
+                Домашняя работа по PHP
+            </h1>
+            <h4 class="subtitle">
+                домашняя работа по PHP - создание страницы player.php и таблицы БД music для него, а также формы для внесения данных в таблицу БД music.
+            </h4>
+        </header>
+        <section class="begin">
+            <p>
+                <b>Задание 1</b><br>
+                Создание старницы/файла php player.php
+            </p>
+            <p>
+                <b>Задание 2</b><br>
+                Создание в MyAdminPHP БД таблицы music со столбцами "name" и "author".<br>
+                В столбце "name" - указываем название композиции.<br>
+                В столбце "author" - указываем исполнителя.
+            </p>
+            <p>
+                <b>Задание 3</b><br>
+                На странице player.php создаем базовую разметку HTML и выводим на ней данные из таблицы БД music.
+            </p>
+            <p>
+                <b>Задание 4</b><br>
+                Создать в player.php HTML форму с полями - "name" и "author" с кнопкой "ДОБАВИТЬ" и написать код php - чтобы можно было добавлять данные в mysql таблицу БД music через данную форму.
+            </p>
+            <p>
+                <b>ВАЖНО!</b><br>
+                Продейнного материала будет не совсем достаточно, чтобы полностью выполнить домашнее задание, поэтому рекомендую обратиться за дополнительной информацией к интернту.
+            </p>
+            <p>
+                По ходу выполнения заданий будут отсылки к ранее пройденному материалу и интернет ресурсам откуда была взята дополнительная информация.
+            </p>
+        </section>
+        <section class=""></section>
+    </main>
+
+</body>
+
+</html>
